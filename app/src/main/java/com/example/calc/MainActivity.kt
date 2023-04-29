fb06309fc11418af0c74cf119b20c67de73482f9
menna <mennaibrahim4691@gmail.com>
2023-04-29 07:41:25 +0300

    Convert variable names to camelCase for consistency and readability.

--- app/src/main/java/com/example/calc/MainActivity.kt
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -8,95 +8,107 @@ import androidx.core.text.isDigitsOnly
 import com.example.calc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var currenttext:String
-    var operation:Boolean=false
-    private var tempresult:Double=0.0
-    private lateinit var resulttext:String
-    private var result:Double=0.0
+    private lateinit var currentText: String
+    private var operation: Boolean = false
+    private var tempResult: Double = 0.0
+    private lateinit var resultText: String
+    private var result: Double = 0.0
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        addCallBacks()
+        addCallbacks()
     }
-    private fun addCallBacks() {
-        binding.buttonBack.setOnClickListener { returnback() }
 
-        binding.buttonBack.setOnLongClickListener{clear()}
-
-        binding.buttonClear.setOnClickListener { cleartext() }
-
-        binding.buttonEqual.setOnClickListener{
+    private fun addCallbacks() {
+        binding.buttonBack.setOnClickListener { returnBack() }
+        binding.buttonBack.setOnLongClickListener{ clear() }
+        binding.buttonClear.setOnClickListener { clearText() }
+        binding.buttonEqual.setOnClickListener {
             updateOperation()
-            currenttext=tempresult.toString()
-            binding.textViewInput.text=currenttext
-            resulttext="0"
-            binding.textViewResult.text=resulttext
-            }
-
-        binding.buttonMul.setOnClickListener{it.displayInputText()
-            operation=true}
-
-        binding.buttonPlus.setOnClickListener{it.displayInputText()
-            operation=true}
-
-        binding.buttonMinus.setOnClickListener{it.displayInputText()
-            operation=true }
-
-        binding.buttonDiv.setOnClickListener{it.displayInputText()
-            operation=true}
-
-        binding.buttonPow.setOnClickListener{it.displayInputText()
-            operation=true}
+            currentText = tempResult.toString()
+            binding.textViewInput.text = currentText
+            resultText = "0"
+            binding.textViewResult.text = resultText
+        }
+        binding.buttonMul.setOnClickListener {
+            it.displayInputText()
+            operation = true
+        }
+        binding.buttonPlus.setOnClickListener {
+            it.displayInputText()
+            operation = true
+        }
+        binding.buttonMinus.setOnClickListener {
+            it.displayInputText()
+            operation = true
+        }
+        binding.buttonDiv.setOnClickListener {
+            it.displayInputText()
+            operation = true
+        }
+        binding.buttonPow.setOnClickListener {
+            it.displayInputText()
+            operation = true
+        }
     }
-    private fun updateOperation(){
-        if(operation)
-            tempresult=result
+
+    private fun updateOperation() {
+        if (operation) {
+            tempResult = result
+        }
     }
-    private fun value(view: View):String{
+
+    private fun value(view: View): String {
         return (view as Button).text.toString()
     }
-    private fun View.displayInputText(){
-        currenttext=binding.textViewInput.text.toString()+value(this)
-        binding.textViewInput.text=currenttext
+
+    private fun View.displayInputText() {
+        currentText = binding.textViewInput.text.toString() + value(this)
+        binding.textViewInput.text = currentText
     }
+
     private fun displayOutputText() {
-        result =calcoulator(currenttext)
-        resulttext=result.toString()
-        binding.textViewResult.text=resulttext
+        result = calculator(currentText)
+        resultText = result.toString()
+        binding.textViewResult.text = resultText
     }
+
     fun click(view: View) {
         view.displayInputText()
         displayOutputText()
     }
-    private fun clear():Boolean{
-        currenttext=""
-        resulttext="0"
-        binding.textViewInput.text=currenttext
-        binding.textViewResult.text=resulttext
+
+    private fun clear(): Boolean {
+        currentText = ""
+        resultText = "0"
+        binding.textViewInput.text = currentText
+        binding.textViewResult.text = resultText
         return true
     }
-    private fun cleartext() {
-        operation=true
-        result=0.0
+
+    private fun clearText() {
+        operation = true
+        result = 0.0
         clear()
     }
-    private fun returnback() {
-        operation=false
-        if(currenttext.length>1) {
-            currenttext = currenttext.dropLast(1)
-            binding.textViewInput.text = currenttext
-            result = if(currenttext.isDigitsOnly())
-                        currenttext.toDouble()
-                    else
-                        calcoulator(currenttext)
+
+    private fun returnBack() {
+        operation = false
+        if (currentText.length > 1) {
+            currentText = currentText.dropLast(1)
+            binding.textViewInput.text = currentText
+            result = if (currentText.isDigitsOnly()) {
+                currentText.toDouble()
+            } else {
+                calculator(currentText)
+            }
             binding.textViewResult.text = result.toString()
-            }
-        else
-            {
-                clear()
-            }
+        } else {
+            clear()
+        }
     }
 }
